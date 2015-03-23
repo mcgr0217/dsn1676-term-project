89ccbdd1857b9bbf2bf26f7683e677ac419f0107
mcgr0217 <Samantha@samanthas-MacBook-Pro.local>
2015-03-23 18:22:26 -0400

    Touch Ups
    Fixing the style of the button and adding the pointer cursor to the
    button

--- js-effects/js/main.js
+++ js-effects/js/main.js
@@ -22,17 +22,4 @@ $tabs.on("click", "a", function () {
 
 $('.nav').localScroll();
 
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
+$('.bar').localScroll();
